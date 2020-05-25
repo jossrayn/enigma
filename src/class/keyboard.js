cf1cd9f3b2f47f36bfebeef5c9ec2e2ff292900e
chechoblack <50933382+chechoblack@users.noreply.github.com>
2020-05-24 20:08:24 -0600

    Class

---
+++ src/class/keyboard.js
@@ -0,0 +1,13 @@
+class keyboard {
+
+    alphabet=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+    constructor(name){
+        this.name=name;
+    }
+
+    alphabet(){
+        
+        return this.alphabet;
+    }
+}
